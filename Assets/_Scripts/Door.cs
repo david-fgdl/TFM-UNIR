fe6342647d010c9171ee143fb64696c79f6de79a
MaCabezon <mcabezonarias@gmail.com>
2022-05-13 17:35:54 +0200

    Poniendo puertas

--- Assets/_Scripts/Door.cs
+++ Assets/_Scripts/Door.cs
@@ -2,15 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "TFM-UNIR/New Door")]
-public class Door : ScriptableObject {
+
+public class Door : MonoBehaviour {
 
     public string Id;
     public string DoorName;
-    public string UnlockObjectId;
+    public InventoryItemData UnlockObject;
     public Puzzle.Type Type;
-    public GameObject Prefab;
-    public bool IsOpen;
+    
+
+    #region Open Variables
+    public bool IsOpen = false;
+    [SerializeField] private Transform pivot;
+    private float closedRotation = 0f;
+    private float currentRotation;
+    private float openedRotation = -87f;
+
+    #endregion
+
+    void Awake()
+    {
+        this.gameObject.name = Id;
+    }
+
+    void Start()
+    {
+        currentRotation = closedRotation;
+    }
+
+    void Update()
+    {
+        OpenClose(IsOpen);
+    }
+
+    void OpenClose(bool isOpen) {
+
+        
+        if (isOpen == true && currentRotation >= openedRotation) {
+            currentRotation-=1f;
+        } else if (isOpen == false && currentRotation < closedRotation) {
+            currentRotation+=1f;
+        }
+
+        pivot.eulerAngles = new Vector3(0, currentRotation, 0);
+    }
     
 }
 
--- Assets/_Scripts/Puzzle.cs
+++ Assets/_Scripts/Puzzle.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Puzzle : ScriptableObject
 {
     public enum Type {
+        None,
         Object,
         Number
     }
