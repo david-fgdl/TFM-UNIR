845cdc23a06dfe6c5bdb84853b1f9c9735cfeda0
MaCabezon <mcabezonarias@gmail.com>
2022-05-18 23:37:17 +0200

    Inventario arreglado

--- Assets/_Scripts/Entities/Player/PlayerController.cs
+++ Assets/_Scripts/Entities/Player/PlayerController.cs
@@ -257,12 +257,19 @@ public class PlayerController : MonoBehaviour
     // ACTION OPEN/CLOSE INVENTORY
     public void Inventory(InputAction.CallbackContext context)
     {
-        if (context.started) {
+        bool performed = false;
+        if (context.started && !performed) {
+            Debug.Log("CURRENT STATE: "+ GameManager.Instance.State);
+
             if (GameManager.Instance.State == GameState.Game)
-                GameManager.Instance.ChangeState(GameState.Inventory); // Inventory closed
+                GameManager.Instance.ChangeState(GameState.Inventory);
             else if (GameManager.Instance.State == GameState.Inventory)
-                GameManager.Instance.ChangeState(GameState.Game); // Inventory open
+                GameManager.Instance.ChangeState(GameState.Game);
+
+            performed = true;
         }
+
+        performed = false;
         
 
     }
