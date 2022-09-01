94de461a1acffb93f254105ad85b668d9e7fb6a7
MaCabezon <mcabezonarias@gmail.com>
2022-09-01 11:54:09 +0200

    Update EnemyFOVEditor.cs

--- Assets/_Scripts/Entities/Enemy/EnemyFOVEditor.cs
+++ Assets/_Scripts/Entities/Enemy/EnemyFOVEditor.cs
@@ -17,8 +17,8 @@ public class EnemyFOVEditor : Editor
         EnemyIA Enemy = (EnemyIA)target;
         Handles.color = Color.white;
         Handles.DrawWireArc(Enemy.transform.position, Vector3.up, Vector3.forward, 360, Enemy.getRadius());
-        Vector3 viewAngle1 = directionFromAngle(Enemy.transform.eulerAngles.y, -Enemy.getAngle() / 2);
-        Vector3 viewAngle2 = directionFromAngle(Enemy.transform.eulerAngles.y, Enemy.getAngle() / 2);
+        Vector3 viewAngle1 = DirectionFromAngle(Enemy.transform.eulerAngles.y, -Enemy.getAngle() / 2);
+        Vector3 viewAngle2 = DirectionFromAngle(Enemy.transform.eulerAngles.y, Enemy.getAngle() / 2);
 
         Handles.color = Color.yellow;
         Handles.DrawLine(Enemy.transform.position, Enemy.transform.position + viewAngle1 * Enemy.getRadius());
@@ -32,7 +32,7 @@ public class EnemyFOVEditor : Editor
     }
 
     // METODO AUXILIAR PARA DIBUJAR LA DIRECCION DESDE EL ANGULO
-    private Vector3 directionFromAngle(float eulerY, float angleInDegrees)
+    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
     {
         angleInDegrees += eulerY;
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
