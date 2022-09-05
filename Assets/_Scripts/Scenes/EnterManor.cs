2127de4e5a834abb6181a73eca2fdbcd06e42cd2
Javier López Sánchez <javil.bow@gmail.com>
2022-09-05 20:21:49 +0200

    Más scripts comentados

--- Assets/_Scripts/Scenes/EnterManor.cs
+++ Assets/_Scripts/Scenes/EnterManor.cs
@@ -1,3 +1,5 @@
+/* SCRIPT PARA CONTROLAR LA ENTRADA EN LA MANSION */
+
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -5,19 +7,27 @@ using UnityEngine;
 
 public class EnterManor : MonoBehaviour
 {
+
+    /* VARIABLES */
+
+    // REFERENCIAS
     [SerializeField] private GameObject _rainObject;
 
+    // FLAGS
     private bool _playerInsideManor = false;
 
+    /* METODOS DE TRIGGER */
+
+    // METODO ON TRIGGER ENTER PARA QUE LA LLUVIA PARE TRAS ENTRAR EN LA MANSION
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"Has chocado con {other.tag}");
         if (other.CompareTag("Player"))
         {
             _playerInsideManor = !_playerInsideManor;
-            // Apagamo la generación de partículas
 
             _rainObject.SetActive(!_playerInsideManor);
         }
-    }   
+    }
+
 }
